73d65a7823e86beef08c9682f98f2d06402466a5
KKamjangE <ajm980718@gmail.com>
2025-04-30 15:16:45 +0900

    Add padding to main content area in RootLayout for improved layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 						<NavLink name="TM" path="/10" />
 					</nav>
 				</header>
-				<main className="px-[55]">{children}</main>
+				<main className="px-[55] pt-[30]">{children}</main>
 			</body>
 		</html>
 	);

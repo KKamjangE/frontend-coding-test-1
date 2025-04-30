dd9463bdb29c2b463b6489da141bcf1eb3001a32
KKamjangE <ajm980718@gmail.com>
2025-04-30 14:28:25 +0900

    Add NotFound component for handling 404 errors

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+ 
+export default function NotFound() {
+  return (
+    <div className="flex flex-col justify-center items-center font-bold text-gray-600">
+      <span className="text-9xl">404</span>
+      <span className="text-2xl">페이지를 찾을 수 없습니다.</span>
+    </div>
+  )
+}
